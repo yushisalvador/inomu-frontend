6beb19812fe159d0c4484ea3106235eff129a391
yccjs <yccjsalvador@gmail.com>
2022-06-22 17:21:32 +0900

    feat: make mobile view for individual post responsive

--- src/components/IndividualPost.js
+++ src/components/IndividualPost.js
@@ -5,7 +5,7 @@ export default function IndividualPost({ setView, postData, selectedPost }) {
     <div>
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        className="h-10 inline-block mr-2 hover:cursor-pointer ml-14 mt-10"
+        className="h-10 inline-block mr-2 hover:cursor-pointer md:ml-14 mt-10"
         onClick={() => setView("feed")}
         fill="none"
         viewBox="0 0 24 24"
@@ -18,7 +18,7 @@ export default function IndividualPost({ setView, postData, selectedPost }) {
           d="M10 19l-7-7m0 0l7-7m-7 7h18"
         />
       </svg>
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center md:w-2/4 p-2">
         <div className="font-medium text-2xl">
           {" "}
           {postData[selectedPost].cocktail_name}
@@ -34,13 +34,13 @@ export default function IndividualPost({ setView, postData, selectedPost }) {
             alt="cocktail"
           />
         </div>
-        <div className="mt-2 mb-4 w-2/4">
+        <div className="mt-2 mb-4">
           {" "}
-          <div>Ingredients : </div>
+          <div className="font-bold">Ingredients : </div>
           <div>{postData[selectedPost].ingredients} </div>
         </div>{" "}
-        <div className="w-2/4">
-          <div> Recipe :</div>
+        <div>
+          <div className="font-bold"> Recipe :</div>
           <div>{postData[selectedPost].recipe}</div>
         </div>
       </div>
