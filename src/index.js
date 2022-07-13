b200abbd98a72333318a9fe0811a528e8598d69e
yccjs <yccjsalvador@gmail.com>
2022-07-13 14:53:15 +0900

    chore: fix deployment issue

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
 import "tw-elements";
-import Login from "./pages/Login";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
